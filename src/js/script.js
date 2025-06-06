9f789f79fc142c1e3dd0925b30db707366b26d0f
vesta228322 <hotabych39@gmail.com>
2025-06-06 16:37:42 +0000

    new foles

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: false,
+        autoplaySpeed: 6500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.svg"></button>'
+    });
+});
