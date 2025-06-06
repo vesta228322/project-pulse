32b3b971b27b6cb8d01a2e98664d0d65d3900337
vesta <hotabych39@gmail.com>
2025-06-07 01:38:13 +0300

    Changes have been made.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
-        autoplay: false,
+        adaptiveHeight: true,
+        autoplay: true,
         autoplaySpeed: 6500,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
