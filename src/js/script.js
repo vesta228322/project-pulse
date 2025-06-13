e67979445a4c24086937e392b0d1f451b7f85216
vesta228322 <hotabych39@gmail.com>
2025-06-13 21:30:42 +0300

    Add new scripts

--- src/js/script.js
+++ src/js/script.js
@@ -36,4 +36,51 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('fast');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('fast');
+        })
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                username: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Введите существующую почту"
+                }
+              }
+        });
+    };
+    validateForms('#consiltation-for');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+375 (99) 999-99-99");
 });
